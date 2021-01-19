ac72b2cefcd2a035c0a24b101b76a14900f4117b
Max <maxhryan@outlook.com>
2021-01-19 14:11:18 -0600

    step 14

---
+++ app.js
@@ -0,0 +1,58 @@
+var myName = "max";
+
+const states = 50;
+
+var addition = 5 + 4;
+
+function sayHello() {
+alert("Hello World!");
+}
+sayHello();
+
+function checkAge(name, age) {
+    if (age < 21) {
+        alert("Sorry " + name + ", you aren't old enough to view this page!");
+    }
+}
+checkAge("charles",21)
+checkAge("abby",27)
+checkAge("james",18)
+checkAge("john",17)
+var names = ["charles","abby","james","john"];
+
+var charles = 21;
+var abby = 27;
+var james = 18;
+var john = 17;
+
+var myVeggies = ["squash", "beets", "cabbage", "carrots"];
+
+for(var i = 0; i < myVeggies.length; i++){
+    console.log(myVeggies[i])
+}
+
+var pet = {
+    name: "Maggie",
+    breed: "black lab"
+}
+console.log(pet);
+
+var people =  ["dave","sally","george","simon","greg"]
+var dave = 24;
+var sally = 16;
+var george = 18;
+var simon = 22;
+var greg = 31;
+
+
+
+for (i = 0; i < people.length; i++){
+    checkAge("dave",24)
+    checkAge("sally",16)
+    checkAge("george",18)
+    checkAge("simon",22)
+    checkAge("greg",31)
+    break
+}
+
+
